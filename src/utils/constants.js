3ecb754d870334626477720400c1f71f58f79bf1
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2020-11-10 09:56:13 +0800

    测试

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -35,7 +35,7 @@ const LOCAL_PATH = process.env.NODE_ENV !== 'production' ? '..' : `../${APP_BASE
 
 const SERVER_PATH = getServerPath();
 
-const MANAGER_CONTEXT = `${SERVER_PATH}/sei-manager/`;
+const MANAGER_CONTEXT = `/sei-manager/`;
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 /** 非菜单页面 */
