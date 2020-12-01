2a8fd40d0d03ba4d29adbcf5807020398027f58f
eason <cheng.yi@capitalcrux.com>
2020-12-01 13:10:19 +0800

    UE

--- src/pages/ApplyOrder/models/ApplyOrder.js
+++ src/pages/ApplyOrder/models/ApplyOrder.js
@@ -2,7 +2,9 @@ import { utils } from 'suid';
 import { constants } from '@/utils';
 
 const { APPLY_ORDER_TYPE } = constants;
-const APPLY_ORDER_TYPE_DATA = Object.keys(APPLY_ORDER_TYPE).map(key => APPLY_ORDER_TYPE[key]);
+const APPLY_ORDER_TYPE_DATA = Object.keys(APPLY_ORDER_TYPE)
+  .map(key => APPLY_ORDER_TYPE[key])
+  .filter(a => a.name !== APPLY_ORDER_TYPE.ALL.name);
 const { dvaModel } = utils;
 const { modelExtend, model } = dvaModel;
 
