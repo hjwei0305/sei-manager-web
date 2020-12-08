d0de73b35e43b66986ae351fa6970b8d6dd4475a
eason <cheng.yi@capitalcrux.com>
2020-12-08 18:06:14 +0800

    UE

--- src/pages/DeployTemplate/components/DeployTemplateForm/Form.js
+++ src/pages/DeployTemplate/components/DeployTemplateForm/Form.js
@@ -1,39 +1,68 @@
 import React, { PureComponent } from 'react';
 import cls from 'classnames';
-import { get } from 'lodash';
+import { get, omit, isEqual } from 'lodash';
 import { FormattedMessage } from 'umi-plugin-react/locale';
 import { Button, Form, Input } from 'antd';
-import { BannerTitle } from 'suid';
+import { BannerTitle, utils } from 'suid';
+import AceEditor from 'react-ace';
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/theme-terminal';
 import styles from './Form.less';
 
+const { getUUID } = utils;
 const FormItem = Form.Item;
 const formItemLayout = {
   labelCol: {
-    span: 6,
+    span: 24,
   },
   wrapperCol: {
-    span: 18,
+    span: 24,
   },
 };
 
 @Form.create()
 class FeatureGroupForm extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.aceId = getUUID();
+    const { templateData } = props;
+    const { globalParam = '{↵    ↵}' } = templateData || {};
+    this.state = {
+      globalParam,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    const { templateData } = this.props;
+    if (!isEqual(prevProps.templateData, templateData)) {
+      const { globalParam = '{↵    ↵}' } = templateData || {};
+      this.setState({ globalParam });
+    }
+  }
+
   handlerFormSubmit = () => {
     const { form, saveDeployTemplate, templateData, handlerPopoverHide } = this.props;
     const { validateFields, getFieldsValue } = form;
+    const { globalParam } = this.state;
     validateFields(errors => {
       if (errors) {
         return;
       }
-      const params = { ...templateData };
+      const params = { globalParam };
+      Object.assign(params, omit(templateData, ['globalParam']) || {});
       Object.assign(params, getFieldsValue());
       saveDeployTemplate(params, handlerPopoverHide);
     });
   };
 
+  handlerAceChannge = globalParam => {
+    this.setState({ globalParam });
+  };
+
   render() {
     const { form, templateData, saving } = this.props;
     const { getFieldDecorator } = form;
+    const { globalParam } = this.state;
     const title = templateData ? '编辑' : '新建';
     return (
       <div key="form-box" className={cls(styles['form-box'])}>
@@ -64,7 +93,29 @@ class FeatureGroupForm extends PureComponent {
                 ],
               })(<Input maxLength={30} />)}
             </FormItem>
-            <FormItem wrapperCol={{ span: 4, offset: 6 }} className="btn-submit">
+            <FormItem label="模板参数(Json对象)">
+              <AceEditor
+                mode="json"
+                theme="terminal"
+                name={this.aceId}
+                fontSize={14}
+                onChange={this.handlerAceChannge}
+                showPrintMargin={false}
+                showGutter={false}
+                highlightActiveLine
+                width="100%"
+                height="120px"
+                value={globalParam}
+                setOptions={{
+                  enableBasicAutocompletion: false,
+                  enableLiveAutocompletion: false,
+                  enableSnippets: true,
+                  showLineNumbers: false,
+                  tabSize: 4,
+                }}
+              />
+            </FormItem>
+            <FormItem wrapperCol={{ span: 4 }} className="btn-submit">
               <Button type="primary" loading={saving} onClick={this.handlerFormSubmit}>
                 <FormattedMessage id="global.save" defaultMessage="保存" />
               </Button>
