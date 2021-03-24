fe9ff4e1e98a321d04c58e787f0eac878824f939
eason <cheng.yi@capitalcrux.com>
2021-03-24 16:31:31 +0800

    UE

--- src/pages/WorkTodo/components/DeployDetail/index.js
+++ src/pages/WorkTodo/components/DeployDetail/index.js
@@ -2,15 +2,13 @@ import React, { PureComponent } from 'react';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { Form, Input, Row, Col, DatePicker } from 'antd';
-import { ExtModal, utils } from 'suid';
-import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-markdown';
-import 'ace-builds/src-noconflict/theme-textmate';
+import { Form, Input, DatePicker, Layout, Card } from 'antd';
+import { ExtModal, ExtIcon, BannerTitle, ScrollBar } from 'suid';
+import { MdEditorView } from '@/components';
 import styles from './index.less';
 
+const { Sider, Content } = Layout;
 const FormItem = Form.Item;
-const { getUUID } = utils;
 const formItemLayout = {
   labelCol: {
     span: 24,
@@ -22,18 +20,21 @@ const formItemLayout = {
 
 @Form.create()
 class DeployDetail extends PureComponent {
-  static aceId;
-
   static propTypes = {
     showModal: PropTypes.bool,
     closeFormModal: PropTypes.func,
     rowData: PropTypes.object,
   };
 
-  constructor(props) {
-    super(props);
-    this.aceId = getUUID();
-  }
+  renderTitle = () => {
+    const { closeFormModal } = this.props;
+    return (
+      <>
+        <ExtIcon onClick={closeFormModal} type="left" className="trigger-back" antd />
+        <BannerTitle title="显示" subTitle="部署申请" />
+      </>
+    );
+  };
 
   render() {
     const { form, rowData, showModal, closeFormModal } = this.props;
@@ -43,127 +44,103 @@ class DeployDetail extends PureComponent {
       <ExtModal
         centered
         destroyOnClose
-        width={860}
         visible={showModal}
         onCancel={closeFormModal}
         wrapClassName={styles['form-box']}
         bodyStyle={{ paddingBottom: 0 }}
-        title="部署申请详情"
+        title={this.renderTitle()}
         footer={null}
       >
-        <Row gutter={8}>
-          <Col span={10}>
-            <div className="item-box">
-              <div className="form-body">
-                <Form {...formItemLayout} layout="horizontal">
-                  <FormItem label="部署主题">
-                    {getFieldDecorator('name', {
-                      initialValue: get(rowData, 'name'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '部署主题不能为空',
-                        },
-                      ],
-                    })(<Input placeholder="请输入部署主题" disabled />)}
-                  </FormItem>
-                  <FormItem label="部署环境">
-                    {getFieldDecorator('envName', {
-                      initialValue: get(rowData, 'envName'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '部署环境不能为空',
-                        },
-                      ],
-                    })(<Input disabled />)}
-                  </FormItem>
-                  <FormItem label="部署应用">
-                    {getFieldDecorator('appName', {
-                      initialValue: get(rowData, 'appName'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '部署应用不能为空',
-                        },
-                      ],
-                    })(<Input disabled />)}
-                  </FormItem>
-                  <FormItem label="模块名称">
-                    {getFieldDecorator('moduleName', {
-                      initialValue: get(rowData, 'moduleName'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '模块名称不能为空',
-                        },
-                      ],
-                    })(<Input disabled />)}
-                  </FormItem>
-                  <FormItem label="标签名称">
-                    {getFieldDecorator('tagName', {
-                      initialValue: get(rowData, 'tagName'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '标签名称不能为空',
-                        },
-                      ],
-                    })(<Input disabled />)}
-                  </FormItem>
-                  <FormItem label="期望完成时间">
-                    {getFieldDecorator('expCompleteTime', {
-                      initialValue: expCompleteTime ? moment(expCompleteTime) : null,
-                      rules: [
-                        {
-                          required: true,
-                          message: '期望完成时间不能为空',
-                        },
-                      ],
-                    })(
-                      <DatePicker
-                        allowClear={false}
-                        style={{ width: '100%' }}
-                        showTime={{ format: 'HH:mm' }}
-                        format="YYYY-MM-DD HH:mm:00"
-                        disabled
-                      />,
-                    )}
-                  </FormItem>
-                </Form>
-              </div>
-            </div>
-          </Col>
-          <Col span={14}>
-            <div className="item-box">
-              <div className="item-label">部署说明(支持Markdown)</div>
-              <div className="item-body">
-                <AceEditor
-                  style={{ marginBottom: 24 }}
-                  mode="markdown"
-                  theme="textmate"
-                  placeholder="请输入部署说明(例如：部署要求,脚本内容)"
-                  name={this.aceId}
-                  fontSize={14}
-                  showPrintMargin={false}
-                  showGutter={false}
-                  readOnly
-                  highlightActiveLine
-                  width="100%"
-                  height="526px"
-                  value={get(rowData, 'remark')}
-                  setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: true,
-                    showLineNumbers: false,
-                    tabSize: 2,
-                  }}
-                />
-              </div>
-            </div>
-          </Col>
-        </Row>
+        <Layout className="auto-height">
+          <Sider width={300} className="auto-height form-box" theme="light">
+            <ScrollBar>
+              <Form {...formItemLayout} layout="horizontal">
+                <FormItem label="部署主题">
+                  {getFieldDecorator('name', {
+                    initialValue: get(rowData, 'name'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '部署主题不能为空',
+                      },
+                    ],
+                  })(<Input placeholder="请输入部署主题" disabled />)}
+                </FormItem>
+                <FormItem label="部署环境">
+                  {getFieldDecorator('envName', {
+                    initialValue: get(rowData, 'envName'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '部署环境不能为空',
+                      },
+                    ],
+                  })(<Input disabled />)}
+                </FormItem>
+                <FormItem label="部署应用">
+                  {getFieldDecorator('appName', {
+                    initialValue: get(rowData, 'appName'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '部署应用不能为空',
+                      },
+                    ],
+                  })(<Input disabled />)}
+                </FormItem>
+                <FormItem label="模块名称">
+                  {getFieldDecorator('moduleName', {
+                    initialValue: get(rowData, 'moduleName'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '模块名称不能为空',
+                      },
+                    ],
+                  })(<Input disabled />)}
+                </FormItem>
+                <FormItem label="标签名称">
+                  {getFieldDecorator('tagName', {
+                    initialValue: get(rowData, 'tagName'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '标签名称不能为空',
+                      },
+                    ],
+                  })(<Input disabled />)}
+                </FormItem>
+                <FormItem label="期望完成时间">
+                  {getFieldDecorator('expCompleteTime', {
+                    initialValue: expCompleteTime ? moment(expCompleteTime) : null,
+                    rules: [
+                      {
+                        required: true,
+                        message: '期望完成时间不能为空',
+                      },
+                    ],
+                  })(
+                    <DatePicker
+                      allowClear={false}
+                      style={{ width: '100%' }}
+                      showTime={{ format: 'HH:mm' }}
+                      format="YYYY-MM-DD HH:mm:00"
+                      disabled
+                    />,
+                  )}
+                </FormItem>
+              </Form>
+            </ScrollBar>
+          </Sider>
+          <Content className="auto-height main-content" style={{ paddingLeft: 8 }}>
+            <Card bordered={false} key="tag-content" className="tag-content" title="部署说明">
+              <MdEditorView
+                key="tag-content-md"
+                message={get(rowData, 'remark') || '<span style="color:#999">暂无数据</span>'}
+              />
+            </Card>
+          </Content>
+        </Layout>
       </ExtModal>
     );
   }
--- src/pages/WorkTodo/components/PublishDetail/index.js
+++ src/pages/WorkTodo/components/PublishDetail/index.js
@@ -1,15 +1,13 @@
 import React, { PureComponent } from 'react';
 import { get } from 'lodash';
 import PropTypes from 'prop-types';
-import { Form, Input, Row, Col } from 'antd';
-import { ExtModal, utils } from 'suid';
-import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-markdown';
-import 'ace-builds/src-noconflict/theme-textmate';
+import { Form, Input, Layout, Card } from 'antd';
+import { ExtModal, ExtIcon, BannerTitle, ScrollBar } from 'suid';
+import { MdEditorView } from '@/components';
 import styles from './index.less';
 
+const { Sider, Content } = Layout;
 const FormItem = Form.Item;
-const { getUUID } = utils;
 const formItemLayout = {
   labelCol: {
     span: 24,
@@ -21,18 +19,21 @@ const formItemLayout = {
 
 @Form.create()
 class PublishDetail extends PureComponent {
-  static aceId;
-
   static propTypes = {
     showModal: PropTypes.bool,
     closeFormModal: PropTypes.func,
     rowData: PropTypes.object,
   };
 
-  constructor(props) {
-    super(props);
-    this.aceId = getUUID();
-  }
+  renderTitle = () => {
+    const { closeFormModal } = this.props;
+    return (
+      <>
+        <ExtIcon onClick={closeFormModal} type="left" className="trigger-back" antd />
+        <BannerTitle title="显示" subTitle="发版申请" />
+      </>
+    );
+  };
 
   render() {
     const { form, rowData, showModal, closeFormModal } = this.props;
@@ -46,93 +47,69 @@ class PublishDetail extends PureComponent {
         onCancel={closeFormModal}
         wrapClassName={styles['form-box']}
         bodyStyle={{ paddingBottom: 0 }}
-        title="发版申请详情"
+        title={this.renderTitle()}
         footer={null}
       >
-        <Row gutter={8}>
-          <Col span={10}>
-            <div className="item-box">
-              <div className="form-body">
-                <Form {...formItemLayout} layout="horizontal">
-                  <FormItem label="发版主题">
-                    {getFieldDecorator('name', {
-                      initialValue: get(rowData, 'name'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '发版主题不能为空',
-                        },
-                      ],
-                    })(<Input autoComplete="off" placeholder="请输入发版主题" disabled />)}
-                  </FormItem>
-                  <FormItem label="发版应用">
-                    {getFieldDecorator('appName', {
-                      initialValue: get(rowData, 'appName'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '发版应用不能为空',
-                        },
-                      ],
-                    })(<Input disabled />)}
-                  </FormItem>
-                  <FormItem label="模块名称">
-                    {getFieldDecorator('moduleName', {
-                      initialValue: get(rowData, 'moduleName'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '模块名称不能为空',
-                        },
-                      ],
-                    })(<Input disabled />)}
-                  </FormItem>
-                  <FormItem label="标签名称">
-                    {getFieldDecorator('refTag', {
-                      initialValue: get(rowData, 'refTag'),
-                      rules: [
-                        {
-                          required: true,
-                          message: '标签名称不能为空',
-                        },
-                      ],
-                    })(<Input disabled />)}
-                  </FormItem>
-                </Form>
-              </div>
-            </div>
-          </Col>
-          <Col span={14}>
-            <div className="item-box">
-              <div className="item-label">发版说明(支持Markdown)</div>
-              <div className="item-body">
-                <AceEditor
-                  style={{ marginBottom: 24 }}
-                  mode="markdown"
-                  theme="textmate"
-                  placeholder="请输入发版说明(例如：部署要求,脚本内容)"
-                  name={this.aceId}
-                  fontSize={14}
-                  onChange={this.handlerAceChannge}
-                  showPrintMargin={false}
-                  showGutter={false}
-                  readOnly
-                  highlightActiveLine
-                  width="100%"
-                  height="526px"
-                  value={get(rowData, 'remark')}
-                  setOptions={{
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true,
-                    enableSnippets: true,
-                    showLineNumbers: false,
-                    tabSize: 2,
-                  }}
-                />
-              </div>
-            </div>
-          </Col>
-        </Row>
+        <Layout className="auto-height">
+          <Sider width={300} className="auto-height form-box" theme="light">
+            <ScrollBar>
+              <Form {...formItemLayout} layout="horizontal">
+                <FormItem label="发版主题">
+                  {getFieldDecorator('name', {
+                    initialValue: get(rowData, 'name'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '发版主题不能为空',
+                      },
+                    ],
+                  })(<Input autoComplete="off" placeholder="请输入发版主题" disabled />)}
+                </FormItem>
+                <FormItem label="发版应用">
+                  {getFieldDecorator('appName', {
+                    initialValue: get(rowData, 'appName'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '发版应用不能为空',
+                      },
+                    ],
+                  })(<Input disabled />)}
+                </FormItem>
+                <FormItem label="模块名称">
+                  {getFieldDecorator('moduleName', {
+                    initialValue: get(rowData, 'moduleName'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '模块名称不能为空',
+                      },
+                    ],
+                  })(<Input disabled />)}
+                </FormItem>
+                <FormItem label="标签名称">
+                  {getFieldDecorator('refTag', {
+                    initialValue: get(rowData, 'refTag'),
+                    rules: [
+                      {
+                        required: true,
+                        message: '标签名称不能为空',
+                      },
+                    ],
+                  })(<Input disabled />)}
+                </FormItem>
+              </Form>
+            </ScrollBar>
+          </Sider>
+          <Content className="auto-height main-content" style={{ paddingLeft: 8 }}>
+            <Card bordered={false} key="tag-content" className="tag-content" title="发版说明">
+              <MdEditorView
+                key="tag-content-md"
+                message={get(rowData, 'remark') || '<span style="color:#999">暂无数据</span>'}
+              />
+            </Card>
+          </Content>
+        </Layout>
       </ExtModal>
     );
   }
